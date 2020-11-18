8a3153abde9b252202a0b55dc0f1a109ced3d48a
incrypto <vpkrishnanand@gmail.com>
2020-11-18 06:40:29 +0530

    vitals form linked to db

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,88 +1,93 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Main from '../views/Main.vue'
-import Home from '../views/Home.vue'
-import NewPatient from '../views/NewPatient.vue'
-import VitalsForm from '../views/VitalsForm.vue'
-import Login from '../views/Login.vue'
-import Meals from '../views/Meals.vue'
-import { fire } from '@/ts/firebase_helper'
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Main from "../views/Main.vue";
+import Home from "../views/Home.vue";
+import NewPatient from "../views/NewPatient.vue";
+import VitalsForm from "../views/VitalsForm.vue";
+import Login from "../views/Login.vue";
+import Meals from "../views/Meals.vue";
+import { fire } from "@/ts/firebase_helper";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    children:[
-
+    path: "/",
+    children: [
       {
-        path: '/',
-        name: 'home',
-        component: Home
+        path: "/",
+        name: "home",
+        component: Home,
       },
 
       {
-        path: '/meals/:rid',
-        name: 'meals',
-        component: Meals
-      },
-    
-      {
-        path: '/patients/add',
-        name: 'patients',
-        component:NewPatient,
+        path: "/meals/:rid",
+        name: "meals",
+        component: Meals,
       },
 
       {
-        path: '/patients/edit/:id',
-        name: 'patients',
-        component:NewPatient,
-        props:{
-          edit:true
+        path: "/patients/add",
+        name: "patients",
+        component: NewPatient,
+      },
+
+
+      {
+        path: "/patients/success",
+        name: "patients",
+        component: NewPatient,
+      },
+
+
+      {
+        path: "/patients/edit/:id",
+        name: "patients",
+        component: NewPatient,
+        props: {
+          edit: true,
         },
       },
 
       {
-        path:  '/patients/vitals',
-        name: 'vitals',
-        component:VitalsForm,
-        
+        path: "/patients/vitals/:id",
+        name: "patients",
+        component: VitalsForm,
+        props: {
+          edit: true,
+        },
       },
       {
-        path: '/about',
-        name: 'Meals',
+        path: "/about",
+        name: "Meals",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Meals.vue')
-      }
+        component: () =>
+          import(/* webpackChunkName: "about" */ "../views/Meals.vue"),
+      },
     ],
 
-   
-    component: Main
+    component: Main,
   },
 
   {
-    path:'/login',
-    name:'login',
-    component:Login,
-
+    path: "/login",
+    name: "login",
+    component: Login,
   },
-
-  
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
-
+  routes,
+});
 
 // GOOD
 router.beforeEach((to, from, next) => {
-  if (to.name !== 'login' && !fire.auth.currentUser) next({ name: 'login' })
-  else next()
-})
+  if (to.name !== "login" && !fire.auth.currentUser) next({ name: "login" });
+  else next();
+});
 
-export default router
+export default router;
--- src/views/VitalsForm.vue
+++ src/views/VitalsForm.vue
@@ -1,246 +1,333 @@
 <template>
   <v-container class="grey--text text--darken-2">
-    <h2>Hemanth</h2>
-
-    <v-row>
-      <v-col sm="2" cols="12"> Sex : Male</v-col>
-      <v-col sm="2" cols="12">Age : 42</v-col>
-      <v-col sm="2" cols="12">Room : 101</v-col>
-      <v-col sm="2" cols="12">Date : 21/11/21</v-col>
+    <v-row class="justify-center align-center" v-if="loading" id="loading">
+      <v-progress-circular
+        v-if="loading"
+        indeterminate
+        color="primary"
+        class="mb-6"
+      ></v-progress-circular>
     </v-row>
+    <div v-else>
+      <v-container v-if="dataError">
+        <v-row class="justify-center align-center" >
+          <div>An error occured please try again later</div>
+        </v-row>
+      </v-container>
 
-    <v-row>
-      <v-col>
-        <h3>Patient ID : {{ patient.id }}</h3></v-col
-      >
-    </v-row>
+      <div v-else>
+        <h2>{{patient.name}}</h2>
 
-    <v-row class="my-2">
-      <v-col> <h3>Vitals Examination</h3></v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" md="3" class="">
-        <v-text-field
-          v-model="patient.vitals.bp"
-          :rules="nameRules"
-          outlined
-          rounded
-          dense
-          label="BP"
-          color="grey"
-          suffix="Mm of Hg"
-          required
-        ></v-text-field>
+        <v-row>
+          <v-col  md="2" cols="12"> Sex : {{patient.sex}}</v-col>
+          <v-col md="2" cols="12">Age : {{patient.age}}</v-col>
+          <v-col  md="2" cols="12">Room : {{patient.room}}</v-col>
+          <v-col  md="2" cols="12">Date : {{patient.date}}</v-col>
+        </v-row>
 
-        <v-text-field
-          v-model="patient.vitals.pulse"
-          :rules="nameRules"
-          outlined
-          rounded
-          dense
-          label="Pulse Rate"
-          suffix="/m"
-          required
-        ></v-text-field>
+        <v-row>
+          <v-col>
+            <h3>Patient ID : {{ patient.id }}</h3></v-col
+          >
+        </v-row>
 
-        <v-text-field
-          v-model="patient.vitals.respRate"
-          :rules="nameRules"
-          outlined
-          rounded
-          dense
-          label="Respiration Rate"
-          suffix="/m"
-          required
-        ></v-text-field>
+        <v-row class="my-2">
+          <v-col> <h3>Vitals Examination</h3></v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="3" class="">
+            <v-text-field
+              v-model="patient.vitals.bp"
+              :rules="nameRules"
+              outlined
+              rounded
+              dense
+              label="BP"
+              color="grey"
+              suffix="Mm of Hg"
+              required
+            ></v-text-field>
 
-        <v-text-field
-          v-model="patient.vitals.sp02"
-          :rules="nameRules"
-          outlined
-          rounded
-          dense
-          label="Sp02"
-          suffix="%"
-          required
-        ></v-text-field>
+            <v-text-field
+              v-model="patient.vitals.pulse"
+              :rules="nameRules"
+              outlined
+              rounded
+              dense
+              label="Pulse Rate"
+              suffix="/m"
+              required
+            ></v-text-field>
 
-        <v-text-field
-          v-model="patient.vitals.grbs"
-          :rules="nameRules"
-          outlined
-          rounded
-          dense
-          label="GRBS"
-          plceholder="XX Mm/dL"
-          suffix="Mm/dL"
-          required
-        ></v-text-field>
-      </v-col>
-    </v-row>
+            <v-text-field
+              v-model="patient.vitals.respRate"
+              :rules="nameRules"
+              outlined
+              rounded
+              dense
+              label="Respiration Rate"
+              suffix="/m"
+              required
+            ></v-text-field>
 
-    <v-row class="my-2">
-      <v-col> <h3>Higher Mental Function</h3></v-col>
-    </v-row>
+            <v-text-field
+              v-model="patient.vitals.sp02"
+              :rules="nameRules"
+              outlined
+              rounded
+              dense
+              label="Sp02"
+              suffix="%"
+              required
+            ></v-text-field>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.higherMentalFunction.levelOfConsiousness"
-          autocomplete="email"
-          label="Level of consiousness"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
+            <v-text-field
+              v-model="patient.vitals.grbs"
+              :rules="nameRules"
+              outlined
+              rounded
+              dense
+              label="GRBS"
+              plceholder="XX Mm/dL"
+              suffix="Mm/dL"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.higherMentalFunction.memory"
-          label="Memory"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
+        <v-row class="my-2">
+          <v-col> <h3>Higher Mental Function</h3></v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.higherMentalFunction.attention"
-          label="Attention"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.higherMentalFunction.levelOfConsiousness"
+              autocomplete="email"
+              label="Level of consiousness"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.higherMentalFunction.speech"
-          label="Speech"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.higherMentalFunction.memory"
+              label="Memory"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.higherMentalFunction.mmse"
-          label="MMSE"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.higherMentalFunction.attention"
+              label="Attention"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-    <v-row class="my-2">
-      <v-col> <h3>Cranial Nerve</h3></v-col>
-    </v-row>
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.higherMentalFunction.speech"
+              label="Speech"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.cranialNerve.motorSystem"
-          label="Motor System"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.higherMentalFunction.mmse"
+              label="MMSE"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.cranialNerve.sensorySystem"
-          label="Sensory System"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
+        <v-row class="my-2">
+          <v-col> <h3>Cranial Nerve</h3></v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.cranialNerve.cerebralSign"
-          label="Cerebral Sign"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.cranialNerve.motorSystem"
+              label="Motor System"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.cranialNerve.cerebralSign"
-          label="Meneningeal Sign"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.cranialNerve.sensorySystem"
+              label="Sensory System"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.cranialNerve.menengialSign"
-          label="Periferal Nerves"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.cranialNerve.cerebralSign"
+              label="Cerebral Sign"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-    <v-row>
-      <v-col cols="12" md="6" class="py-0 px-3">
-        <v-text-field
-          v-model="patient.cranialNerve.periferalNerves"
-          label="Skull And Spine"
-          outlined
-          dense
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-bottom-navigation fixed app>
-      <v-spacer></v-spacer>
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.cranialNerve.cerebralSign"
+              label="Meneningeal Sign"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
 
-      <v-btn value="recent">
-        <h3>Save</h3>
-      </v-btn>
-      <v-btn value="recent">
-        <h3>Next</h3>
-      </v-btn>
-      <v-btn value="recent" v-on:click="onClick">
-        <h3>Blah</h3>
-      </v-btn>
-    </v-bottom-navigation>
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.cranialNerve.menengialSign"
+              label="Periferal Nerves"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12" md="6" class="py-0 px-3">
+            <v-text-field
+              v-model="patient.cranialNerve.periferalNerves"
+              label="Skull And Spine"
+              outlined
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-bottom-navigation fixed app>
+          <v-spacer></v-spacer>
+
+          <v-btn value="recent" v-on:click="submit">
+            <h3>Save</h3>
+          </v-btn>
+          <v-btn value="recent">
+            <h3>Next</h3>
+          </v-btn>
+          <v-btn value="recent" v-on:click="onClick">
+            <h3>Blah</h3>
+          </v-btn>
+        </v-bottom-navigation>
+      </div>
+    </div>
+    <v-snackbar v-model="snackBar" :timeout="2000">
+      {{ message }}
+    </v-snackbar>
   </v-container>
 </template>
 <script lang="ts">
-import { Patient } from "@/ts/models/models";
+import { store } from "@/ts/firebase_helper";
+import { Patient, PatientInterface } from "@/ts/models/models";
 import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 
 @Component({})
 export default class VitalForm extends Vue {
   @Prop({
-    type: Object,
-    default: () => new Patient(),
+    type: Boolean,
+    default: false,
   })
-  patient!: Patient;
+  edit!: boolean;
+
+  patient: PatientInterface = new Patient();
+
   modal = false;
   valid = true;
-  nameRules = [(v: any) => !!v || "Email is required"];
+  snackBar = false;
+  message = "";
+  loading = true;
+  dataError = false;
+  submitLoading = false;
+  nameRules = [(v: any) => !!v || "field is required"];
+
+  async created() {
+    console.log("Created called");
+    const id = this.$route.params.id;
+
+    await this.getPatient(this.$route.params.id);
+  }
+
+  async getPatient(id: string) {
+    console.log("----get patient----");
+    this.loading = true;
+    try {
+      if (id) {
+        const patient = await store.getPatient(id);
+        this.patient = patient;
+      }
+    } catch (error) {
+      console.log(error);
+      this.dataError = true;
+    }
+    this.loading = false;
+    console.log("----get patient end----");
+  }
+
+  showMessage(msg: string) {
+    this.message = msg;
+    this.snackBar = true;
+  }
 
   onClick() {
     console.log("HEY BUDDIES");
     console.log(JSON.stringify(this.patient));
   }
+
+  async submit(save = false) {
+    this.submitLoading = true;
+    try {
+     
+        const success = await store.editPatient(
+          this.$route.params.id,
+          this.patient
+        );
+        if (success) {
+          if (save) {
+            this.showMessage("Successfully saved");
+          } else {
+            this.$router.push({ path: `/patients/success/${this.$route.params.id}` });
+          }
+        }
+      
+    } catch (error) {
+      console.log(error);
+      this.showMessage("An error occured");
+    }
+
+    this.submitLoading = false;
+  }
 }
 </script>
+<style scoped>
+#loading {
+  height: 100vh;
+}
+</style>
